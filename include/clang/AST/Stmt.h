djasper/clang

commit 55fa4c1901788f520f23054b218e0baabec2f0aa

[ms-inline asm] Add clobbers to AST representation.
git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@161686 91177308-0d34-0410-b5e6-96231b3b80d8

include/clang/AST/Stmt.h
@@ -1628,16 +1628,18 @@ class MSAsmStmt : public Stmt {
 
   unsigned NumAsmToks;
   unsigned NumLineEnds;
+  unsigned NumClobbers;
 
   Token *AsmToks;
   unsigned *LineEnds;
   Stmt **Exprs;
+  std::string *Clobbers;
 
 public:
   MSAsmStmt(ASTContext &C, SourceLocation asmloc, bool issimple,
             bool isvolatile, ArrayRef<Token> asmtoks,
             ArrayRef<unsigned> lineends, StringRef asmstr,
-            SourceLocation endloc);
+            ArrayRef<std::string> clobbers, SourceLocation endloc);
 
   SourceLocation getAsmLoc() const { return AsmLoc; }
   void setAsmLoc(SourceLocation L) { AsmLoc = L; }
@@ -1661,6 +1663,9 @@ public:
   void setAsmString(StringRef &E) { AsmStr = E.str(); }
 
   //===--- Other ---===//
+
+  unsigned getNumClobbers() const { return NumClobbers; }
+  StringRef getClobber(unsigned i) { return Clobbers[i]; }
 
   SourceRange getSourceRange() const LLVM_READONLY {
     return SourceRange(AsmLoc, EndLoc);

lib/AST/Stmt.cpp
@@ -586,10 +586,11 @@ AsmStmt::AsmStmt(ASTContext &C, SourceLocation asmloc, bool issimple,
 MSAsmStmt::MSAsmStmt(ASTContext &C, SourceLocation asmloc,
                      bool issimple, bool isvolatile, ArrayRef<Token> asmtoks,
                      ArrayRef<unsigned> lineends, StringRef asmstr,
-                     SourceLocation endloc)
+                     ArrayRef<std::string> clobbers, SourceLocation endloc)
   : Stmt(MSAsmStmtClass), AsmLoc(asmloc), EndLoc(endloc),
     AsmStr(asmstr.str()), IsSimple(issimple), IsVolatile(isvolatile),
-    NumAsmToks(asmtoks.size()), NumLineEnds(lineends.size()) {
+    NumAsmToks(asmtoks.size()), NumLineEnds(lineends.size()),
+    NumClobbers(clobbers.size()) {
 
   AsmToks = new (C) Token[NumAsmToks];
   for (unsigned i = 0, e = NumAsmToks; i != e; ++i)
@@ -598,6 +599,10 @@ MSAsmStmt::MSAsmStmt(ASTContext &C, SourceLocation asmloc,
   LineEnds = new (C) unsigned[NumLineEnds];
   for (unsigned i = 0, e = NumLineEnds; i != e; ++i)
     LineEnds[i] = lineends[i];
+
+  Clobbers = new (C) std::string[NumClobbers];
+  for (unsigned i = 0, e = NumClobbers; i != e; ++i)
+    Clobbers[i] = clobbers[i];
 }
 
 ObjCForCollectionStmt::ObjCForCollectionStmt(Stmt *Elem, Expr *Collect,

lib/Sema/SemaStmt.cpp
@@ -2883,6 +2883,7 @@ StmtResult Sema::ActOnMSAsmStmt(SourceLocation AsmLoc,
                                 SourceLocation EndLoc) {
   // MS-style inline assembly is not fully supported, so emit a warning.
   Diag(AsmLoc, diag::warn_unsupported_msasm);
+  SmallVector<std::string,4> Clobbers;
 
   // Empty asm statements don't need to instantiate the AsmParser, etc.
   if (AsmToks.empty()) {
@@ -2890,7 +2891,7 @@ StmtResult Sema::ActOnMSAsmStmt(SourceLocation AsmLoc,
     MSAsmStmt *NS =
       new (Context) MSAsmStmt(Context, AsmLoc, /* IsSimple */ true,
                               /* IsVolatile */ true, AsmToks, LineEnds,
-                              AsmString, EndLoc);
+                              AsmString, Clobbers, EndLoc);
     return Owned(NS);
   }
 
@@ -2937,7 +2938,7 @@ StmtResult Sema::ActOnMSAsmStmt(SourceLocation AsmLoc,
 
   MSAsmStmt *NS =
     new (Context) MSAsmStmt(Context, AsmLoc, IsSimple, /* IsVolatile */ true,
-                            AsmToks, LineEnds, AsmString, EndLoc);
+                            AsmToks, LineEnds, AsmString, Clobbers, EndLoc);
 
   return Owned(NS);
 }

commit 0a1fb96b9fb421a073cf1ec71a29263fcf0c423d

[ms-inline asm] Fix a memory leak introduced in r161686.
git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@161698 91177308-0d34-0410-b5e6-96231b3b80d8

include/clang/AST/Stmt.h
@@ -1633,13 +1633,13 @@ class MSAsmStmt : public Stmt {
   Token *AsmToks;
   unsigned *LineEnds;
   Stmt **Exprs;
-  std::string *Clobbers;
+  StringRef **Clobbers;
 
 public:
   MSAsmStmt(ASTContext &C, SourceLocation asmloc, bool issimple,
             bool isvolatile, ArrayRef<Token> asmtoks,
             ArrayRef<unsigned> lineends, StringRef asmstr,
-            ArrayRef<std::string> clobbers, SourceLocation endloc);
+            ArrayRef<StringRef> clobbers, SourceLocation endloc);
 
   SourceLocation getAsmLoc() const { return AsmLoc; }
   void setAsmLoc(SourceLocation L) { AsmLoc = L; }
@@ -1665,7 +1665,7 @@ public:
   //===--- Other ---===//
 
   unsigned getNumClobbers() const { return NumClobbers; }
-  StringRef getClobber(unsigned i) { return Clobbers[i]; }
+  StringRef *getClobber(unsigned i) { return Clobbers[i]; }
 
   SourceRange getSourceRange() const LLVM_READONLY {
     return SourceRange(AsmLoc, EndLoc);

lib/AST/Stmt.cpp
@@ -586,7 +586,7 @@ AsmStmt::AsmStmt(ASTContext &C, SourceLocation asmloc, bool issimple,
 MSAsmStmt::MSAsmStmt(ASTContext &C, SourceLocation asmloc,
                      bool issimple, bool isvolatile, ArrayRef<Token> asmtoks,
                      ArrayRef<unsigned> lineends, StringRef asmstr,
-                     ArrayRef<std::string> clobbers, SourceLocation endloc)
+                     ArrayRef<StringRef> clobbers, SourceLocation endloc)
   : Stmt(MSAsmStmtClass), AsmLoc(asmloc), EndLoc(endloc),
     AsmStr(asmstr.str()), IsSimple(issimple), IsVolatile(isvolatile),
     NumAsmToks(asmtoks.size()), NumLineEnds(lineends.size()),
@@ -600,9 +600,14 @@ MSAsmStmt::MSAsmStmt(ASTContext &C, SourceLocation asmloc,
   for (unsigned i = 0, e = NumLineEnds; i != e; ++i)
     LineEnds[i] = lineends[i];
 
-  Clobbers = new (C) std::string[NumClobbers];
-  for (unsigned i = 0, e = NumClobbers; i != e; ++i)
-    Clobbers[i] = clobbers[i];
+  Clobbers = new (C) StringRef*[NumClobbers];
+  for (unsigned i = 0, e = NumClobbers; i != e; ++i) {
+    // FIXME: Avoid the allocation/copy if at all possible.
+    size_t size = clobbers[i].size();
+    char *dest = new (C) char[size];
+    std::strncpy(dest, clobbers[i].data(), size); 
+    Clobbers[i] = new (C) StringRef(dest, size);
+  }
 }
 
 ObjCForCollectionStmt::ObjCForCollectionStmt(Stmt *Elem, Expr *Collect,

lib/Sema/SemaStmt.cpp
@@ -2883,7 +2883,7 @@ StmtResult Sema::ActOnMSAsmStmt(SourceLocation AsmLoc,
                                 SourceLocation EndLoc) {
   // MS-style inline assembly is not fully supported, so emit a warning.
   Diag(AsmLoc, diag::warn_unsupported_msasm);
-  SmallVector<std::string,4> Clobbers;
+  SmallVector<StringRef,4> Clobbers;
 
   // Empty asm statements don't need to instantiate the AsmParser, etc.
   if (AsmToks.empty()) {